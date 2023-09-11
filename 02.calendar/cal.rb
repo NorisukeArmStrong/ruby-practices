3c8cf4e47d14c27cdc1371892023d0257338f525
norit <noritomo0424@yahoo.co.jp>
2023-09-11 11:18:55 +0900

    change 'unless' to 'or', '%7' to '% 7'

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -10,14 +10,8 @@ opt.on('-m [VAL]', Integer, 'mon option') {|v| option[:m] = v }
 opt.on('-y [VAL]', Integer, 'year option') {|v| option[:y] = v }
 opt.parse(ARGV)
 
-mon = option[:m]
-unless mon 
-  mon = Date.today.mon
-end
-year = option[:y]
-unless year
-  year = Date.today.year
-end
+mon = option[:m] || mon = Date.today.mon
+year = option[:y] || year = Date.today.year
 
 first_day_wday = Date.new(year, mon, 1).wday
 last_day = Date.new(year, mon, -1).day
@@ -32,7 +26,7 @@ print "   " * first_day_wday
 (1..last_day).each do |date|
   print date.to_s.rjust(2) + " "
   first_day_wday += 1
-  if first_day_wday %7 == 0 || last_day == date
+  if first_day_wday % 7 == 0 || last_day == date
     print "\n"
   end
 end
