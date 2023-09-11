311e953074e6849282e3da539f256e483cd07784
norit <noritomo0424@yahoo.co.jp>
2023-09-11 11:39:41 +0900

    change 'first_day_wday' to 'first_day', 'date' to 'day'

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -13,20 +13,20 @@ opt.parse(ARGV)
 mon = option[:m] || mon = Date.today.mon
 year = option[:y] || year = Date.today.year
 
-first_day_wday = Date.new(year, mon, 1).wday
+first_day = Date.new(year, mon, 1).wday
 last_day = Date.new(year, mon, -1).day
-option_date = Date.new(year, mon, 1)
-head = option_date.strftime("%B") + " #{year}"
+option_day = Date.new(year, mon, 1)
+head = option_day.strftime("%B") + " #{year}"
 week = "Su Mo Tu We Th Fr Sa"
 
 puts head.center(20)
 puts week
-print "   " * first_day_wday
+print "   " * first_day
 
-(1..last_day).each do |date|
-  print date.to_s.rjust(2) + " "
-  first_day_wday += 1
-  if first_day_wday % 7 == 0 || last_day == date
+(1..last_day).each do |day|
+  print day.to_s.rjust(2) + " "
+  first_day += 1
+  if first_day % 7 == 0 || last_day == day
     print "\n"
   end
 end
