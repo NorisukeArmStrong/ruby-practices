b96dbd40bc6c2c173ea539fbc4e3803bc57bbd9c
norit <noritomo0424@yahoo.co.jp>
2023-09-13 12:08:12 +0900

    rename first_day to first_day_wday. remove mon and year from duplicate

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -10,10 +10,10 @@ opt.on('-m [VAL]', Integer, 'mon option') {|v| option[:m] = v }
 opt.on('-y [VAL]', Integer, 'year option') {|v| option[:y] = v }
 opt.parse(ARGV)
 
-mon = option[:m] || mon = Date.today.mon
-year = option[:y] || year = Date.today.year
+mon = option[:m] || Date.today.mon
+year = option[:y] || Date.today.year
 
-first_day = Date.new(year, mon, 1).wday
+first_day_wday = Date.new(year, mon, 1).wday
 last_day = Date.new(year, mon, -1).day
 option_day = Date.new(year, mon, 1)
 head = option_day.strftime("%B") + " #{year}"
@@ -21,11 +21,11 @@ week = "Su Mo Tu We Th Fr Sa"
 
 puts head.center(20)
 puts week
-print "   " * first_day
+print "   " * first_day_wday
 
 (1..last_day).each do |day|
   print day.to_s.rjust(2) + " "
-  first_day += 1
+  first_day_wday += 1
   if first_day % 7 == 0 || last_day == day
     print "\n"
   end
