3c9f984c44b7df4cf7a012c2e6c79d5a09f52374
norit <noritomo0424@yahoo.co.jp>
2023-09-16 10:59:26 +0900

    ボウリングのスコア計算プログラムを作成

---
+++ 03.bowling/bowling.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+score = ARGV[0]
+scores = score.split(',')
+shots = []
+scores.each do |s|
+  if s == 'X'
+    shots << 10
+    shots << 0
+  else
+    shots << s.to_i
+  end
+end
+
+frames = []
+shots.each_slice(2) do |s|
+  frames << s
+end
+
+point = 0
+frames.each do |frame|
+  point += frame.sum
+end
+puts point
