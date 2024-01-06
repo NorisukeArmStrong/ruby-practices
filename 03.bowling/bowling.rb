22b73e9ddcb561b7b9dd17f70152afd8e5f09285
norit <noritomo0424@yahoo.co.jp>
2024-01-06 19:52:22 +0900

    引数を参照して合計得点を計算する機能を追加

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,29 +1,31 @@
 #!/usr/bin/env ruby
 
-score = ARGV[0]
-scores = score.split(',')
 shots = []
-scores.each do |s|
-  if s == 'X'
+scores = ARGV[0].split(',').each do |score|
+  if score == 'X'
     shots << 10
+    shots << 0
   else
-    shots << s.to_i
+    shots << score.to_i
   end
 end
 
 frames = []
-shots.each_slice(2) do |s|
-  frames << s
+shots.each_slice(2) do |shot|
+  frames << shot
 end
 
 point = 0
-frames.each do |frame|
-  if frame[0] == 10
-    point += 30
-  elsif frame.sum == 10
-    point += frame[0] + 10
+# binding.irb
+frames.each_with_index do |frame, i|
+  if frame[0] == 10 && i < 9
+    point += (frames[i] + frames[i + 1]).sum
+  elsif frame.sum == 10 && i < 9
+    point += frames[i].sum + frames[i + 1].first
   else
     point += frame.sum
   end
 end
 puts point
+
+p frames
