00c960a96865df44714b4269f4f4d150200576a0
norit <noritomo0424@yahoo.co.jp>
2024-01-08 05:45:03 +0900

    引数に特定の値が連続したとき正しく計算される機能を追加

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 shots = []
-scores = ARGV[0].split(',').each do |score|
+ARGV[0].split(',').each do |score|
   if score == 'X'
     shots << 10
     shots << 0
@@ -16,16 +16,17 @@ shots.each_slice(2) do |shot|
 end
 
 point = 0
-# binding.irb
 frames.each_with_index do |frame, i|
   if frame[0] == 10 && i < 9
     point += (frames[i] + frames[i + 1]).sum
+    if frames[i + 1][0] == 10 && i < 9
+      point += (frames[i + 2]).sum
+    else
+      point += 0
+    end
   elsif frame.sum == 10 && i < 9
     point += frames[i].sum + frames[i + 1].first
   else
     point += frame.sum
   end
 end
-puts point
-
-p frames
