bdb8724d00efa5edbeed9f2c277d44df5ad11daf
norit <noritomo0424@yahoo.co.jp>
2024-01-18 06:19:14 +0900

    マジックナンバーを定数化、またその定数名を修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -2,12 +2,11 @@
 # frozen_string_literal: true
 
 shots = []
-STRIKE_1 = 10
-STRIKE_2 = 0
+STRIKE_SCORE = 10
 ARGV[0].split(',').each do |score|
   if score == 'X'
-    shots << STRIKE_1
-    shots << STRIKE_2
+    shots << STRIKE_SCORE
+    shots << 0
   else
     shots << score.to_i
   end
@@ -20,16 +19,16 @@ end
 
 point = 0
 frames.each_with_index do |frame, i|
-  if frame[0] == 10 && i < 9
+  if frame[0] == STRIKE_SCORE && i < 9
     point += (frames[i] + frames[i + 1]).sum
-    point += if frames[i + 1][0] == 10 && i < 9 && frames[i + 2][0] != 10
+    point += if frames[i + 1][0] == STRIKE_SCORE && i < 9 && frames[i + 2][0] != STRIKE_SCORE
                frames[i + 2].first
-             elsif frames[i + 1][0] == 10 && i < 9
+             elsif frames[i + 1][0] == STRIKE_SCORE && i < 9
                (frames[i + 2]).sum
              else
                0
              end
-  elsif frame.sum == 10 && i < 9
+  elsif frame.sum == STRIKE_SCORE && i < 9
     point += frames[i].sum + frames[i + 1].first
   else
     point += frame.sum
