890bac38a71ed6e5124a8a345277b12bba8eb2bf
norisuke <noritomo0424@yahoo.co.jp>
2025-09-15 23:24:49 +0900

    FizzBuzz再訓練

--- 01.fizzbuzz/fizz_buzz.rb
+++ 01.fizzbuzz/fizz_buzz.rb
@@ -1,16 +1,26 @@
 #!/usr/bin/env ruby
 
-number = 0
-20.times do
+20.times do |number|
   number += 1
-    case 
-    when number % 3 == 0 && number % 5 == 0
-      puts "FizzBuzz"
-    when number % 3 == 0
-      puts "Fizz"
-    when number % 5 == 0
-      puts "Buzz"
+  if number % 3 == 0 && number % 5 == 0
+    puts "FizzBuzz"
+  else
+  puts number
+  end
+end
+
+puts "-------------"
+
+(1..20).each do |num|
+  puts (
+    if(num % 3).zero? && (num % 5).zero?
+      "FizzBuzz"
+    elsif (num % 3).zero?
+      "Fizz"
+    elsif (num % 5).zero?
+      "Buzz"
     else
-      puts number
+      num
     end
+  )
 end
